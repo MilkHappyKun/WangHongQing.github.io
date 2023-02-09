2dd667c7d1ec3d3cccbe36e474575deb54ffbbd3
wanghongqing <1977428570@qq.com>
2023-02-09 18:21:14 +0800

    更新管理系统

--- assets/User.19eea907.js
+++
@@ -1,1 +0,0 @@
-import{s,h as n,C as r,o,c as l,t as c,u}from"./index.65ef0bc3.js";const p={__name:"User",setup(_){let e=s({});n(()=>{t()});let t=async()=>{let a=await r();e.value=a.data};return(a,f)=>(o(),l("div",null,c(u(e)),1))}};export{p as default};
